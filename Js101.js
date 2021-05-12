6a9332d7a5716e62148a99de68ddb1dd99483cab
satawatearth <satawat.eart@mail.kmutt.ac.th>
2021-05-12 16:47:01 +0700

    v2

---
+++ Js101.js
@@ -0,0 +1,11 @@
+var num = [1,2,3,4,5];
+
+let x = num.map((n) => 6 * n);
+x = [10,20,30,40,50]
+
+document.write(x);
+
+var ages = [32, 33, 16, 40, 10, 5, 18];
+
+var x = ages.filter((a) => a <= 32)
+
